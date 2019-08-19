ff1a2bcaf2dee9c478d54651cee595a01bdf0a77
endkarn <end_karn@hotmail.com>
2019-08-19 14:32:21 +0700

    [Edited]: Refactor โค้ดให้ทดสอบผ่าน

--- tennis/court.go
+++ tennis/court.go
@@ -14,20 +14,11 @@ func (court Court) GetScore() string {
 		2:"30",
 		3:"40",
 	}
-
-	var score string
-	score = "LOVE-"
-	if court.PlayerTwoScore == 1 {
-		return score+scoreMap[1]
-	}
-	if court.PlayerTwoScore == 2 {
-		return score+scoreMap[2]
-	}
-	if court.PlayerTwoScore == 3 {
-		return score+scoreMap[3]
-	}
 	if court.PlayerTwoScore == 4 {
 		return "Player 2 Win"
 	}
-	return "LOVE-LOVE"
+	if court.PlayerOneScore == 4 {
+		return "Player 1 Win"
+	}
+	return scoreMap[court.PlayerOneScore]+"-"+scoreMap[court.PlayerTwoScore]
 }
