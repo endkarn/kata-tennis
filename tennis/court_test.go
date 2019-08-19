18f3dbe6d7e68f58366e63802b61c431d5c8e3f3
endkarn <end_karn@hotmail.com>
2019-08-19 14:15:22 +0700

    [Added]: UnitTest ของฟังชั่น GetScore()

--- tennis/court_test.go
+++ tennis/court_test.go
@@ -19,11 +19,53 @@ func Test_GetScore_Input_Court_PlayerOneScore_0_Court_PlayerTwoScore_0_Should_Be
 	}
 }
 
-func Test_GetScore_Input_Court_PlayerOneScore_0_Court_PlayerTwoScore_1_Should_Be_LOVE_LOVE(t *testing.T){
+func Test_GetScore_Input_Court_PlayerOneScore_0_Court_PlayerTwoScore_1_Should_Be_LOVE_15(t *testing.T){
 	expected := "LOVE-15"
 	court := tennis.Court{
 		PlayerOneScore:0,
 		PlayerTwoScore:1,
+	}
+
+	actual := court.GetScore()
+
+	if expected != actual {
+		t.Errorf("Expected in %s but get %s ",expected,actual)
+	}
+}
+
+func Test_GetScore_Input_Court_PlayerOneScore_0_Court_PlayerTwoScore_2_Should_Be_LOVE_30(t *testing.T){
+	expected := "LOVE-30"
+	court := tennis.Court{
+		PlayerOneScore:0,
+		PlayerTwoScore:2,
+	}
+
+	actual := court.GetScore()
+
+	if expected != actual {
+		t.Errorf("Expected in %s but get %s ",expected,actual)
+	}
+}
+
+func Test_GetScore_Input_Court_PlayerOneScore_0_Court_PlayerTwoScore_3_Should_Be_LOVE_40(t *testing.T){
+	expected := "LOVE-40"
+	court := tennis.Court{
+		PlayerOneScore:0,
+		PlayerTwoScore:3,
+	}
+
+	actual := court.GetScore()
+
+	if expected != actual {
+		t.Errorf("Expected in %s but get %s ",expected,actual)
+	}
+}
+
+func Test_GetScore_Input_Court_PlayerOneScore_0_Court_PlayerTwoScore_4_Should_Be_Player_2_Win(t *testing.T){
+	expected := "Player 2 Win"
+	court := tennis.Court{
+		PlayerOneScore:0,
+		PlayerTwoScore:4,
 	}
 
 	actual := court.GetScore()
