b57910cbba4b9d92d4a9f32e0376a57f7254c079
endkarn <end_karn@hotmail.com>
2019-08-19 14:24:27 +0700

    [Edited]: Refactor โค้ดของ func GetScore

--- tennis/court.go
+++ tennis/court.go
@@ -5,15 +5,26 @@ type Court struct {
 	PlayerTwoScore int
 }
 
+
+
 func (court Court) GetScore() string {
+	scoreMap := map[int]string{
+		0:"LOVE",
+		1:"15",
+		2:"30",
+		3:"40",
+	}
+
+	var score string
+	score = "LOVE-"
 	if court.PlayerTwoScore == 1 {
-		return "LOVE-"+"15"
+		return score+scoreMap[1]
 	}
 	if court.PlayerTwoScore == 2 {
-		return "LOVE-"+"30"
+		return score+scoreMap[2]
 	}
 	if court.PlayerTwoScore == 3 {
-		return "LOVE-"+"40"
+		return score+scoreMap[3]
 	}
 	if court.PlayerTwoScore == 4 {
 		return "Player 2 Win"
