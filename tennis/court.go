cb38a13272ef012f10311acfde10f85aaae62d2d
endkarn <end_karn@hotmail.com>
2019-08-19 14:17:54 +0700

    [Edited]: รับปรุง func SourceCode ให้ Test ผ่าน

--- tennis/court.go
+++ tennis/court.go
@@ -6,5 +6,17 @@ type Court struct {
 }
 
 func (court Court) GetScore() string {
+	if court.PlayerTwoScore == 1 {
+		return "LOVE-"+"15"
+	}
+	if court.PlayerTwoScore == 2 {
+		return "LOVE-"+"30"
+	}
+	if court.PlayerTwoScore == 3 {
+		return "LOVE-"+"40"
+	}
+	if court.PlayerTwoScore == 4 {
+		return "Player 2 Win"
+	}
 	return "LOVE-LOVE"
 }
